615a1722bff064678eba71392f28b861010affa9
Michał Pawłowski <kontakt@michalpawlowski.pl>
2026-06-28 23:59:31 +0200

    feat: add Header, Search and Category components

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,7 +1,7 @@
-export function Header() {
+export function Categori() {
   return (
     <header className="px-6 py-4 border-b border-gray-200 text-center">
-      <span className="text-xl font-medium">🍴 FoodExplorer</span>
+      <span className="text-xl font-medium">Lista Kategorii</span>
     </header>
   );
 }
---
+++ src/components/Search.tsx
@@ -0,0 +1,15 @@
+export function Search() {
+  return (
+    <div className="flex w-full max-w-xl gap-2 p-15 bg-blue-50">
+      <input
+        type="text"
+        placeholder="Wyszukaj przepis"
+        // onChange={(e) => onSearch(e.target.value)}
+        className="flex-1 px-5 py-3 rounded-xl text-sm bg-white outline-none shadow"
+      />
+      <button className="px-5 py-3 bg-green-500 hover:bg-green-400 text-white text-sm rounded-xl transition-colors">
+        Szukaj
+      </button>
+    </div>
+  );
+}
